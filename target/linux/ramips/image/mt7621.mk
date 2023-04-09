21d9a605cebbfee87a1acac7f4a8e15e214f5c02
langliang3241 <49267148+langliang3241@users.noreply.github.com>
2023-04-09 15:23:42 +0800

    闪存利用最大
    IMAGE_SIZE := 32448k

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -971,7 +971,7 @@ TARGET_DEVICES += jcg_y2
 
 define Device/jdcloud_re-sp-01b
   $(Device/dsa-migration)
-  IMAGE_SIZE := 27328k
+  IMAGE_SIZE := 32448k
   DEVICE_VENDOR := JDCloud
   DEVICE_MODEL := RE-SP-01B
   DEVICE_PACKAGES := kmod-fs-ext4 kmod-mt7603 \
